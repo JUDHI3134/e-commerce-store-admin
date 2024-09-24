42daaab19b8bf1f5ba08ed65e20b7a53d3f014e1
JUDHI3134 <judhistirbehera241@gmail.com>
2024-09-24 09:54:31 +0530

    create if token available show pages if not show login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './components/Navbar'
 import Sidebar from './components/Sidebar'
 import { Route, Routes } from 'react-router-dom'
 import Add from './pages/Add'
 import List from './pages/List'
 import Orders from './pages/Orders'
+import Login from './components/Login'
 
 const App = () => {
+  const [token,setToken] = useState('')
   return (
     <div className='bg-gray-50 min-h-screen'>
-    <>
+
+    {token === "" ? <Login /> :
+     <>
       <Navbar />
       <hr />
       <div className='flex w-full'>
@@ -22,7 +26,7 @@ const App = () => {
              </Routes>
         </div>
       </div>
-    </>
+    </>}
     </div>
   )
 }
