7d6bbdf7d1cf79a2dab5ce7c6574b5a73be97b60
JUDHI3134 <judhistirbehera241@gmail.com>
2024-09-24 18:46:19 +0530

    create remove product button in list page

--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from 'react'
 import { backendUrl, currency } from '../App'
 import { toast } from 'react-toastify'
 
-const List = () => {
+const List = ({token}) => {
 
     const [list,setList] = useState([])
 
@@ -20,6 +20,22 @@ const List = () => {
          toast.error(error.message)
        }
        
+    }
+
+    const removeProduct = async (id) =>{
+        try {
+            const response = await axios.post(backendUrl + "/api/product/remove",{id},{headers: {token}});
+            if(response.data.success){
+                toast.success(response.data.message)
+                fetchList()
+            }
+            else{
+                toast.error(response.data.message)
+            }
+        } catch (error) {
+            console.log(error);
+            toast.error(error.message)
+        }
     }
 
     useEffect(()=>{
@@ -48,7 +64,7 @@ const List = () => {
                     <p>{item.name}</p>
                     <p>{item.category}</p>
                     <p>{currency}{item.price}</p>
-                    <p className='text-right md:text-center cursor-pointer text-lg'>X</p>
+                    <p onClick={()=>removeProduct(item._id)} className='text-right md:text-center cursor-pointer text-lg'>X</p>
                 </div>
             ))
         }
