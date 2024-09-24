264702856322bb04a1d39ff2a16824819297639a
JUDHI3134 <judhistirbehera241@gmail.com>
2024-09-24 11:28:29 +0530

    create add product page ui

--- src/pages/Add.jsx
+++ src/pages/Add.jsx
@@ -1,10 +1,95 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 const Add = () => {
   return (
-    <div>
-      
-    </div>
+    <form className='flex flex-col items-start w-full gap-3'>
+      <div>
+        <p className='mb-2'>Upload Image</p>
+        <div className='flex gap-2'>
+            <label htmlFor="image1">
+                <img className='w-20' src={assets.upload_area} alt="" />
+                <input type="file" id="image1" hidden />
+            </label>
+            <label htmlFor="image2">
+                <img className='w-20' src={assets.upload_area} alt="" />
+                <input type="file" id="image2" hidden />
+            </label>
+            <label htmlFor="image3">
+                <img className='w-20' src={assets.upload_area} alt="" />
+                <input type="file" id="image3" hidden />
+            </label>
+            <label htmlFor="image4">
+                <img className='w-20' src={assets.upload_area} alt="" />
+                <input type="file" id="image4" hidden />
+            </label>
+        </div>
+      </div>
+
+      <div className='w-full'>
+        <p className='mb-2'>Product Name</p>
+        <input className='w-full max-w-[500px] px-3 py-2' type="text" placeholder='Type here..' required />
+      </div>
+      <div className='w-full'>
+        <p className='mb-2'>Product Description</p>
+        <textarea className='w-full max-w-[500px] px-3 py-2' type="text" placeholder='Write content here..' required />
+      </div>
+
+      <div className='flex flex-col gap-2 sm:flex-row w-full sm:gap-8'>
+
+        <div>
+            <p className='mb-2'>Product Category</p>
+            <select className='w-full px-3 py-2'>
+                <option value="Men">Men</option>
+                <option value="Women">Women</option>
+                <option value="Kid">Kid</option>
+            </select>
+        </div>
+        <div>
+            <p className='mb-2'>Sub Category</p>
+            <select className='w-full px-3 py-2'>
+                <option value="Topwear">Topwear</option>
+                <option value="Bottomwear">Bottomwear</option>
+                <option value="Winterwear">Winterwear</option>
+            </select>
+        </div>
+
+        <div>
+            <p className='mb-2'>Product Price</p>
+            <input className='w-full px-3 py-2 sm:w-[120px]' type="number" placeholder='20' />
+        </div>
+
+      </div>
+
+
+        <div>
+        <p className='mb-2'>Product Sizes</p>
+            <div className='flex gap-3'>
+                <div>
+                    <p className='bg-slate-200 px-3 py-1 cursor-pointer'>S</p>
+                </div>
+                <div>
+                    <p className='bg-slate-200 px-3 py-1 cursor-pointer'>M</p>
+                </div>
+                <div>
+                    <p className='bg-slate-200 px-3 py-1 cursor-pointer'>L</p>
+                </div>
+                <div>
+                    <p className='bg-slate-200 px-3 py-1 cursor-pointer'>XL</p>
+                </div>
+                <div>
+                    <p className='bg-slate-200 px-3 py-1 cursor-pointer'>XXL</p>
+                </div>
+            </div>
+        </div>
+
+        <div className='flex gap-2 mt-2'>
+            <input type="checkbox" id='besrseller' />
+            <label label='cursor-pointer' htmlFor="bestseller">Add to bestseller</label>
+        </div>
+        <button className='w-28 py-3 mt-4 bg-green-600 text-white' type="submit">ADD</button>
+
+    </form>
   )
 }
 
