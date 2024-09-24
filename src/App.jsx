5acc0009651d5c66e5d67a88521a3d7069e56ba1
JUDHI3134 <judhistirbehera241@gmail.com>
2024-09-24 09:50:21 +0530

    create navbar sidebar route setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import Sidebar from './components/Sidebar'
+import { Route, Routes } from 'react-router-dom'
+import Add from './pages/Add'
+import List from './pages/List'
+import Orders from './pages/Orders'
 
 const App = () => {
   return (
+    <div className='bg-gray-50 min-h-screen'>
     <>
       <Navbar />
+      <hr />
+      <div className='flex w-full'>
+        <Sidebar/>
+        <div className='w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base'>
+             <Routes>
+              <Route path='/add' element={<Add/>}/>
+              <Route path='/list' element={<List/>}/>
+              <Route path='/orders' element={<Orders/>}/>
+             </Routes>
+        </div>
+      </div>
     </>
+    </div>
   )
 }
 
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+import { assets } from '../assets/assets'
 
 const Sidebar = () => {
   return (
-    <div>
-      
+    <div className='w-[18%] min-h-screen border-r-2 shadow'>
+      <div className='flex flex-col gap-4 pt-6 pl-[20%] text-[15px]'>
+        <NavLink className="flex items-center gap-3 border border-green-300 border-r-0 px-3 py-2 rounded" to="/add">
+            <img className='h-5 w-5' src={assets.add_icon} alt="" />
+            <p className='hidden md:block'>Add product</p>
+        </NavLink>
+        <NavLink className="flex items-center gap-3 border border-green-300 border-r-0 px-3 py-2 rounded" to="/list">
+            <img className='h-5 w-5' src={assets.order_icon} alt="" />
+            <p className='hidden md:block'>List product</p>
+        </NavLink>
+        <NavLink className="flex items-center gap-3 border border-green-300 border-r-0 px-3 py-2 rounded" to="/orders">
+            <img className='h-5 w-5' src={assets.order_icon} alt="" />
+            <p className='hidden md:block'>Orders</p>
+        </NavLink>
+      </div>
     </div>
   )
 }
