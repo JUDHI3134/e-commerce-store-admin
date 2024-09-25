2e39f12b631c80834c36b92e22e802c6bdd99c4a
JUDHI3134 <judhistirbehera241@gmail.com>
2024-09-25 14:38:53 +0530

    create order page ui and display all orders using backend

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,11 +1,102 @@
-import React from 'react'
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import axios from "axios";
+import { backendUrl, currency } from "../App";
+import { toast } from "react-toastify";
+import { assets } from "../assets/assets";
 
-const Orders = () => {
+const Orders = ({ token }) => {
+  const [orders, setOrders] = useState([]);
+
+  const fetchAllOrders = async () => {
+    if (!token) {
+      return null;
+    }
+
+    try {
+      const response = await axios.post(
+        backendUrl + "/api/order/list",
+        {},
+        { headers: { token } }
+      );
+      if (response.data.success) {
+        setOrders(response.data.orders);
+      } else {
+        toast.error(response.data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchAllOrders();
+  }, [token]);
+
   return (
     <div>
-      
+      <h3>Orders Page</h3>
+      <div>
+        {orders.map((order, index) => (
+          <div className="grid grid-cols-1 sm:grid-cols-[0.5fe_2fr_1fr] lg:grid-cols-[0.5fr_2fr_1fr_1fr_1fr] gap-3 items-start border-2 border-green-200 p-5 md:p-8 my-3 md:my-4 text-xs sm:text-sm text-gray-700" key={index}>
+            <img className="w-12" src={assets.parcel_icon} alt="" />
+            <div>
+              <div>
+                {order.items.map((item, index) => {
+                  if (index === order.items.length - 1) {
+                    return (
+                      <p className="py-0.5" key={index}>
+                        {item.name} X {item.quantity} <span>{item.size}</span>{" "}
+                      </p>
+                    );
+                  } else {
+                    return (
+                      <p className="py-0.5" key={index}>
+                        {item.name} X {item.quantity} <span>{item.size}</span>,{" "}
+                      </p>
+                    );
+                  }
+                })}
+              </div>
+              <p className="mt-3 mb-2 font-medium">{order.address.firstName + " " + order.address.lastName}</p>
+              <div>
+                <p>{order.address.street + ","}</p>
+                <p>
+                  {order.address.city +
+                    ", " +
+                    order.address.state +
+                    ", " +
+                    order.address.country +
+                    ", " +
+                    order.address.zipcode}
+                </p>
+              </div>
+              <p>{order.address.phone}</p>
+            </div>
+            <div>
+              <p className="text-sm sm:text-[15px]">Items:{order.items.length}</p>
+              <p className="mt-3">Method: {order.paymentMethod}</p>
+              <p>Payment: {order.payment ? "done" : "Pending"}</p>
+              <p>Date: {new Date(order.date).toLocaleDateString()}</p>
+            </div>
+            <p className="text-sm sm:text-[15px]">
+              {currency}
+              {order.amount}
+            </p>
+            <select className="p-2 font-semibold">
+              <option value="Order Placed">Order Placed</option>
+              <option value="Packing">Packing</option>
+              <option value="Shipped">Shipped</option>
+              <option value="Out for delivery">Out for delivery</option>
+              <option value="Delivered">Delivered</option>
+            </select>
+          </div>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Orders
+export default Orders;
